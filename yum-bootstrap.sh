8ac2679aecf7e797428a60363f429a0f12d0027b
snickerjp <snickerjp@gmail.com>
2016-03-04 18:37:18 +0900

    modify wget loop

--- yum-bootstrap.sh
+++ yum-bootstrap.sh
@@ -20,6 +20,9 @@ rpm -q --whatprovides yumconf 2>&1 >/dev/null
 if [ $? != 0 ]; then
     if [ ! -f $BSDIR/redhat-yumconf-4-4.8.el4.nosrc.rpm ]; then
         wget --no-check-certificate -O $BSDIR/redhat-yumconf-4-4.8.el4.nosrc.rpm "https://wiki.centos.org/HowTos/PackageManagement/YumOnRHEL?action=AttachFile&do=get&target=redhat-yumconf-4-4.8.el4.nosrc.rpm"
+        if [ $? != 0 ]; then
+           wget -O $BSDIR/redhat-yumconf-4-4.8.el4.nosrc.rpm "https://wiki.centos.org/HowTos/PackageManagement/YumOnRHEL?action=AttachFile&do=get&target=redhat-yumconf-4-4.8.el4.nosrc.rpm"
+        fi
     fi
     if [ ! -f /usr/src/redhat/SOURCES/RHEL-Base.repo ]; then
         wget -O /usr/src/redhat/SOURCES/RHEL-Base.repo http://public-yum.oracle.com/public-yum-el4.repo
@@ -39,19 +42,32 @@ export ARCH=`uname -i`
 #export SOURCE=http://vault.centos.org/4.7/os/$ARCH/CentOS/RPMS
 export SOURCE=http://vault.centos.org/4.9/os/$ARCH/CentOS/RPMS
 
-wget -P $BSDIR -nH $SOURCE/yum-2.4.3-4.el4.centos.noarch.rpm
-wget -P $BSDIR -nH $SOURCE/yum-metadata-parser-1.0-8.el4.centos.$ARCH.rpm
-wget -P $BSDIR -nH $SOURCE/python-urlgrabber-2.9.8-2.noarch.rpm
-wget -P $BSDIR -nH $SOURCE/python-elementtree-1.2.6-5.el4.centos.$ARCH.rpm
-wget -P $BSDIR -nH $SOURCE/python-sqlite-1.1.7-1.2.1.$ARCH.rpm
-wget -P $BSDIR -nH $SOURCE/sqlite-3.3.6-2.$ARCH.rpm
-wget -P $BSDIR -nH $SOURCE/libxml2-python-2.6.16-12.6.$ARCH.rpm
-wget -P $BSDIR -nH $SOURCE/rpm-python-4.3.3-32_nonptl.$ARCH.rpm
-#wget -P $BSDIR -nH $SOURCE/rpm-4.3.3-32_nonptl.$ARCH.rpm
-#wget -P $BSDIR -nH $SOURCE/rpm-libs-4.3.3-32_nonptl.$ARCH.rpm
-#wget -P $BSDIR -nH $SOURCE/rpm-build-4.3.3-32_nonptl.$ARCH.rpm
-#rpm -Uvh $BSDIR/yum* $BSDIR/python* $BSDIR/sqlite*
-rpm -Uvh $BSDIR/python* $BSDIR/sqlite* $BSDIR/rpm* $BSDIR/libxml2* $BSDIR/yum*
+export YUM_PKG_LIST=`cat << EOS
+yum-2.4.3-4.el4.centos.noarch.rpm
+yum-metadata-parser-1.0-8.el4.centos.$ARCH.rpm
+python-urlgrabber-2.9.8-2.noarch.rpm
+python-elementtree-1.2.6-5.el4.centos.$ARCH.rpm
+python-sqlite-1.1.7-1.2.1.$ARCH.rpm
+sqlite-3.3.6-2.$ARCH.rpm
+libxml2-python-2.6.16-12.6.$ARCH.rpm
+rpm-python-4.3.3-32_nonptl.$ARCH.rpm
+elfutils-0.97.1-5.$ARCH.rpm
+elfutils-libelf-0.97.1-5.$ARCH.rpm
+popt-1.9.1-32_nonptl.$ARCH.rpm
+rpm-4.3.3-32_nonptl.$ARCH.rpm
+rpm-libs-4.3.3-32_nonptl.$ARCH.rpm
+rpm-build-4.3.3-32_nonptl.$ARCH.rpm
+EOS`
+
+# download loop
+for i in `echo $YUM_PKG_LIST`
+    do if [!-f $i]; then
+           wget -P $BSDIR -nH $SOURCE/$i
+       fi
+done
+
+# rpm setup
+rpm -Uvh $BSDIR/popt* $BSDIR/elfutils* $BSDIR/python* $BSDIR/sqlite* $BSDIR/rpm* $BSDIR/libxml2* $BSDIR/yum*
 if [ $? -eq 0 ]; then
         rm -rf $BSDIR
 fi
